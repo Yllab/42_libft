59095e7f290071cf279eb4c79cad2d239f5db31e
Yllab <bally.hugo@gmail.com>
2018-12-21 16:25:07 +0100

    debugging atol.c

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,21 +6,23 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 20:00:42 by hbally            #+#    #+#             */
-/*   Updated: 2018/11/14 18:36:04 by hbally           ###   ########.fr       */
+/*   Updated: 2018/12/20 17:46:26 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int				ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int ret;
+	int 		i;
+	int 		sign;
+	int 		ret;
+	int			previous;
 
 	i = 0;
 	sign = 1;
 	ret = 0;
+	previous = 0;
 	while ((str[i] >= 7 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	sign = str[i] == '-' ? -1 : 1;
@@ -28,6 +30,9 @@ int		ft_atoi(const char *str)
 	while (ft_isdigit(str[i]))
 	{
 		ret = ret * 10 + (str[i] - '0');
+		if (ret < previous && (ret < 0 && sign == -1 ?  (ret - 1) < 0 : 1))
+			return (0);
+		previous = ret;
 		i++;
 	}
 	return (ret * sign);
