971928ef9027bc57d27fa2148d9ef5e1fe1718b0
hbally <hbally@student.42.fr>
2018-12-29 19:31:30 +0100

    working on dtoa // temporary change to str_realloc

--- srcs/ft_str_realloc.c
+++ srcs/ft_str_realloc.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 19:15:59 by hbally            #+#    #+#             */
-/*   Updated: 2018/12/29 19:25:18 by hbally           ###   ########.fr       */
+/*   Updated: 2018/12/29 19:27:52 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_str_realloc(char *old, size_t size)
 	unsigned int	i;
 	char			*new;
 
-	if (old != NULL)
+	if (old)
 	{
-		new = (char*)malloc(++size);
-		if (new == NULL)
+		new = (char*)ft_memalloc(++size);
+		if (!new)
 			return (NULL);
 		i = 0;
 		while (old[i] && i < size - 1)
@@ -30,7 +30,7 @@ char	*ft_str_realloc(char *old, size_t size)
 		}
 		while (i++ < size)
 			new[i - 1] = '\0';
-//		free(old);
+		free(old);
 		return (new);
 	}
 	return (NULL);
