c0e668be7c5ef735294ae43adedf2580f6954389
hbally <hbally@student.42.fr>
2018-12-29 19:16:55 +0100

    working on dtoa

--- srcs/ft_dtoa.c
+++ srcs/ft_dtoa.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/22 18:20:34 by hbally            #+#    #+#             */
-/*   Updated: 2018/12/29 19:15:33 by hbally           ###   ########.fr       */
+/*   Updated: 2018/12/29 19:16:52 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,8 @@ static char		*fractoa(double *fract_part, size_t p, char *int_string) // why fra
 		*fract_part -= (double)digit;
 	}
 	concat_result = ft_strjoin(int_string, fract_string);
-	free(int_string);
-	free(fract_string);
+//	free(int_string);
+//	free(fract_string);
 	return (concat_result);
 }
 
