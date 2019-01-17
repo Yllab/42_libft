5db7d52f9aeeac15a7cb66d3d41679211a4859d4
hbally <hbally@student.42.fr>
2019-01-17 17:52:08 +0100

    strlen doesnt crash on NULL input

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 16:25:08 by hbally            #+#    #+#             */
-/*   Updated: 2018/11/08 16:57:51 by hbally           ###   ########.fr       */
+/*   Updated: 2019/01/17 17:51:33 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t len;
 
+	if (!s)
+		return (0);
 	len = 0;
 	while (s[len])
 		len++;
