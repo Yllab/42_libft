b358b30c4cbf1e73a40e97717922cada8779c181
hbally <hbally@student.42.fr>
2019-01-17 14:42:19 +0100

    working on bug

--- libs/libftprintf/srcs/printers.c
+++ libs/libftprintf/srcs/printers.c
@@ -6,7 +6,7 @@
 /*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 15:13:36 by hbally            #+#    #+#             */
-/*   Updated: 2019/01/14 14:39:32 by hbally           ###   ########.fr       */
+/*   Updated: 2019/01/17 14:41:13 by hbally           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,15 @@ void				write_buff(const char *to_add, size_t len, t_index *params)
 {
 	if (len > 0)
 	{
-//		printf("In write_buff : head_old is %zu, head is %zu, len(to_add) is %zu\n", params->head_old, params->head, len);
-
 		params->head_old = params->head;
 		params->head = params->head + len;
-
-//		printf("In write_buff : Updated : head_old is %zu, head is %zu\n", params->head_old, params->head);
-
 		if (params->asprintf && params->buf)
 			if ((params->buf = ft_str_realloc(params->buf, params->head)))
-			{
-//				printf("In write_buff : realloced with len %zu\n", params->head);
-//				printf("In write_buff : copying %zu at index %zu\n", len, params->head_old);
-
 				ft_strncpy(&(params->buf[params->head_old]), to_add, len);
-			}
 		if (!params->asprintf)
 			write(1, to_add, len);
 	}
 }
-
 
 /*
 **	--- printer_fmt() ---
@@ -57,8 +46,6 @@ void				printer_fmt(const char *format,
 								t_index *params,
 								va_list *args)
 {
-//	printf("In printer_fmt : fmt_head is %zu\n", params->fmt_head);//
-//	printf("In printer_fmt : Entering write_buff\n");//
 	write_buff(&(format[params->fmt_head_old]),
 				params->fmt_head - params->fmt_head_old,
 				params);
@@ -66,45 +53,10 @@ void				printer_fmt(const char *format,
 	{
 		parser(format, params, args);
 		params->fmt_head_old = params->fmt_head + 1;
-//		printf("In printer_fmt : updated fmt_head_old > %zu\n", params->fmt_head_old);//
 	}
 	else
-	{
-//		printf("In printer_fmt : End of fmt\n");//
 		params->fmt_head_old = 0;
-	}
 }
-
-/*
-**	--- printer_filler() ---
-**	Prints filler chars ' ' or '0' when needed by width
-**	or precision.
-*/
-
-void				printer_filler(char c, long long len, t_index *params)
-{
-	char			*buffer;
-	int				i;
-
-	if (len <= 999999)
-	{
-		buffer = ft_strnew(len);
-		if (buffer)
-		{
-			i = 0;
-			while (i < len)
-			{
-				buffer[i] = c;
-				i++;
-			}
-			write_buff(buffer, len, params);
-			free(buffer);
-		}
-		else
-			params->buf = NULL;
-	}
-}
-
 
 /*
 **	--- printer_arg() ---
@@ -158,3 +110,32 @@ void				printer_arg(const char *s, const char c, t_index *params)
 		width(s, c, params, 1);
 }
 
+/*
+**	--- printer_filler() ---
+**	Prints filler chars ' ' or '0' when needed by width
+**	or precision.
+*/
+
+void				printer_filler(char c, long long len, t_index *params)
+{
+	char			*buffer;
+	int				i;
+
+	if (len <= 999999)
+	{
+		buffer = ft_strnew(len);
+		if (buffer)
+		{
+			i = 0;
+			while (i < len)
+			{
+				buffer[i] = c;
+				i++;
+			}
+			write_buff(buffer, len, params);
+			free(buffer);
+		}
+		else
+			params->buf = NULL;
+	}
+}
